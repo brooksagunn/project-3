287e704de7bef540f44b764d9ca7232fd3797514
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-27 14:04:15 -0400

    Added Form component with email, username, password hooks

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function Form() {
+    const [ email, setEmail ] = useState('');
+    const [ username, setUserName ] = useState('');
+    const [ password, setPassword ] = useState('');
+}
