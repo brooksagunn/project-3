9430304c954511470ecf67bf9b1e839e7d74188e
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-27 13:18:40 -0400

    Init form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import Nav from './components/Nav/Nav';
 import Header from './components/Header/Header';
 import Section from './components/Section/Section';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
     return (
-      <div>
-        <Nav />
-        <Header />
-        <Section />
-      </div>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <Header/>
+            </Route>
+            <Route exact path="/seasons">
+              <Section />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
 }
