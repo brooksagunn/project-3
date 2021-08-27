488319bd6f9ef754f0e02f193cd830794c03c433
Will <willstruder@gmail.com>
2021-08-27 13:19:33 -0400

    Updated homepage with paragraph and mobile responsive properties

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import Nav from './components/Nav/Nav';
 import Header from './components/Header/Header';
 import Section from './components/Section/Section';
+import Paragraph from './components/Paragraph/Paragraph';
 
 export default function App() {
     return (
       <div>
         <Nav />
         <Header />
+        <Paragraph />
         <Section />
       </div>
     );
