eb9b4cd402de3d8d3424e007942079db728ba488
Michael Callahan <mcallahan0@yahoo.com>
2021-08-28 17:24:28 -0400

    Changed button on Bag page to say Checkout

--- client/src/components/Bag/Bag.jsx
+++ client/src/components/Bag/Bag.jsx
@@ -7,7 +7,7 @@ export default function Bag() {
     return (
         
         <div className="title">
-        <button class="waves-effect waves-light btn"><i class="material-icons right"></i>Add to Bag</button>
+        <button class="waves-effect waves-light btn"><i class="material-icons right"></i>Checkout</button>
         </div>
 
         
