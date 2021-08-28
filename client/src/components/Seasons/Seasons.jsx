4f4314c0196b370e32c1c3c3fd680a87f5a6d192
Will <willstruder@gmail.com>
2021-08-28 18:17:09 -0400

    Edited bg picture in photoshop, imported to assets, corrected className in Bag.jsx, created/styled grid element in Seasons for products

--- client/src/components/Seasons/Seasons.jsx
+++ client/src/components/Seasons/Seasons.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import './Seasons.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-export default function Seasons() {
-    return(
-        <section>
-            <h3>Spring</h3>
-        </section>
-    );
+
+         
+    
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    position: 'absolute',
+  },
+  
+  paper: {
+    padding: theme.spacing(20),
+    textAlign: 'center',
+    top: '240px',
+    left: '10px',
+    width: '50vh',
+    position: 'relative',
+    height: '50vh',
+    opacity: '85%',
+    backgroundColor: '#0f0f0f',
+    color: '#ffffff',
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
+
