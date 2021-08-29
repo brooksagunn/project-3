d9bf6b3f35917d129e6720fede053182de9e9ddf
Will <willstruder@gmail.com>
2021-08-28 23:15:27 -0400

    Created and styled titles for product cards, created and styled footer cards

--- client/src/components/Seasons/Seasons.jsx
+++ client/src/components/Seasons/Seasons.jsx
@@ -5,6 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 
+
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
     display: 'flex',  
@@ -28,14 +29,44 @@ const useStyles = makeStyles((theme) => ({
     color: '#fff',
     backgroundColor: '#1c1c1c',
     opacity: '90%',
+    borderRadius: '10px',
   },
 
   grid: {
       position: 'relative',
-      height: '50vh',
+      height: '45vh',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center'
+  },
+
+  photoTitle: {
+    backgroundColor: '#fff',
+    opacity: '85%',
+    color: '#1c1c1c',
+    position: 'fixed',
+    display: 'flex',
+    top: '110px',
+    justifyContent: 'center',
+    padding: '8px',
+    borderRadius: '8px',
+    fontFamily: 'Alumni Sans',
+    fontSize: '30px',
+    fontWeight: 'bold'
+  },
+
+  photoFooter: {
+    backgroundColor: '#fff',
+    opacity: '85%',
+    color: '#1c1c1c',
+    position: 'fixed',
+    display: 'flex',
+    bottom: '12px',
+    justifyContent: 'center',
+    width: '250px',
+    padding: '8px',
+    borderRadius: '8px',
+    fontFamily: 'Alumni Sans',
   }
 
 }));
@@ -44,27 +75,48 @@ export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
-    <section className={classes.gridContainer}>
-      <Grid container spacing={0}>
+      
+<div>     
+    
+        <section className={classes.gridContainer}>
+            
+        <Grid container spacing={0}>
 
-        <Grid className={classes.grid} item xs={3} sm={3}>
-          <Paper className={classes.paper}>product img here</Paper>
+            <Grid className={classes.grid} item xs={3} sm={3}>
+            <div className={classes.photoTitle}>/ winter /</div>
+            <Paper className={classes.paper}>product img here</Paper>
+            <div className={classes.photoFooter}> Designed for colder climates, this capsule<br></br>
+            includes the perfect winter wear that will<br></br>
+            also leave your wallet warm and fuzzy.</div>
+            </Grid>
+
+            <Grid className={classes.grid} item xs={3} sm={3}>
+            <div className={classes.photoTitle}>/ spring /</div>
+            <Paper className={classes.paper}>product img here</Paper>
+            <div className={classes.photoFooter}> Designed for colder climates, this capsule<br></br>
+            includes the perfect winter wear that will<br></br>
+            also leave your wallet warm and fuzzy.</div>
+            </Grid>
+
+            <Grid className={classes.grid} item xs={3} sm={3}>
+            <div className={classes.photoTitle}>\ summer \</div>
+            <Paper className={classes.paper}>product img here</Paper>
+            <div className={classes.photoFooter}> Designed for colder climates, this capsule<br></br>
+            includes the perfect winter wear that will<br></br>
+            also leave your wallet warm and fuzzy.</div>
+            </Grid>
+
+            <Grid className={classes.grid} item xs={3} sm={3}>
+            <div className={classes.photoTitle}>\ fall \</div>
+            <Paper className={classes.paper}>product img here</Paper>
+            <div className={classes.photoFooter}> Designed for colder climates, this capsule<br></br>
+            includes the perfect winter wear that will<br></br>
+            also leave your wallet warm and fuzzy.</div>
+            </Grid>
+
         </Grid>
-
-        <Grid className={classes.grid} item xs={3} sm={3}>
-          <Paper className={classes.paper}>product img here</Paper>
-        </Grid>
-
-        <Grid className={classes.grid} item xs={3} sm={3}>
-          <Paper className={classes.paper}>product img here</Paper>
-        </Grid>
-
-        <Grid className={classes.grid} item xs={3} sm={3}>
-          <Paper className={classes.paper}>product img here</Paper>
-        </Grid>
-
-      </Grid>
-    </section>
+        </section>
+</div>
   );
 }
 
