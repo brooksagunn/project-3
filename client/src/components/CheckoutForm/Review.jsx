1928f284fc32b0b2a57f674071c65ec6b33377dc
Michael Callahan <mcallahan0@yahoo.com>
2021-08-29 08:40:57 -0400

    Added Review.jsx for order summary and .env file

---
+++ client/src/components/CheckoutForm/Review.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
+
+export default function Review() {
+    return (
+        <>
+            <Typography variant="h6" gutterBottom>Order Summary</Typography>
+            <List disablePadding>
+                {/* list of items in bag */}
+            </List>
+        </>
+    );
+}
