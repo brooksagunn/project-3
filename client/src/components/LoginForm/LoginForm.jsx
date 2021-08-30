82f02949d51dd166deca5fab3df799ff26ee0bc2
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-30 15:50:37 -0400

    Created login form with loginHandler

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,91 +1,78 @@
-// const [firstName, setFirstName] = useState('');
-//   const [lastName, setLastName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Login.css';
 
-//   const signUpHandler = (e) => {
-//     e.preventDefault();
 
-//     axios.post("/api/users", {
-//       firstName: firstName,
-//       lastName: lastName,
-//       email: email,
-//       password: password,
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-//   }
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-//   return (
+  const loginHandler = (e) => {
+    e.preventDefault();
+
+    axios.post('/login', async (req, res) => {
+        try {
+          const userData = await User.findOne({ where: { username: req.body.username } });
+      
+          if (!userData) {
+            res
+              .status(400)
+              .json({ message: 'Incorrect username, please try again' });
+            return;
+          }
+      
+          const validPassword = await userData.checkPassword(req.body.password);
+      
+          if (!validPassword) {
+            res
+              .status(400)
+              .json({ message: 'Incorrect password, please try again' });
+            return;
+          }
+      
+          req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            
+            res.json({ user: userData, message: 'You are now logged in!' });
+          });
+      
+        } catch (err) {
+          res.status(400).json(err);
+        }
+      });
+  }
+
+  return (
         
   
-//     <div class= "login">
-      
-//       <div class="row title">
+    <div>
+      <div class="row title">
 
-//         <form class="col s12" onSubmit={signUpHandler}>
+      <form class="col s12" onSubmit={loginHandler}>
+        <div class="row email">
+          <div class="input-field col s6 push-s3">
+            <input id="emailSignUp" type="email" class="validate" name="email" value={email} onChange={(e) => {
+              setEmail(e.target.value);
+            }}></input>
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div class="row password">
+          <div class="input-field col s6 push-s3">
+            <input id="passwordSignUp" type="password" class="validate" name="password" value={password} onChange={(e) => {
+              setPassword(e.target.value);
+            }}></input>
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div className="title">
+            <button class="waves-effect waves-light btn"><i class="material-icons right"></i>Log In</button>
+        </div>
+      </form>
+    </div>  
 
-//           <div class="row names">
-//             <div class="input-field col s3 push-s3">
-//               <input id="first_name" type="text" class="validate" name="firstName" value={firstName} onChange={(e) => {
-//                 setFirstName(e.target.value);
-//               }}></input>
-//               <label for="first_name">First Name</label>
-//             </div>
-//           </div>
-          
-//           <div className="row names">
-//             <div class="input-field col s3 push-s3">
-//               <input id="last_name" type="text" class="validate" name="lastName" value={lastName} onChange={(e) => {
-//                 setLastName(e.target.value);
-//               }}></input>
-//               <label for="last_name">Last Name</label>
-//             </div>
-//           </div>
+  </div>
 
-//           <div class="row email">
-//             <div class="input-field col s6 push-s3">
-//               <input id="emailSignUp" type="email" class="validate" name="email" value={email} onChange={(e) => {
-//                 setEmail(e.target.value);
-//               }}></input>
-//               <label for="email">Email</label>
-//             </div>
-//           </div>
-
-//           <div class="row password">
-//             <div class="input-field col s6 push-s3">
-//               <input id="passwordSignUp" type="password" class="validate" name="password" value={password} onChange={(e) => {
-//                 setPassword(e.target.value);
-//               }}></input>
-//               <label for="password">Password</label>
-//             </div>
-//           </div>
-
-//           <div className="title">
-//               <button class="waves-effect waves-light btn" type="submit"><i class="material-icons right"></i>Sign Up</button>
-//           </div>
-
-//           <div class="row email">
-//             <div class="input-field col s6 push-s3">
-//               <input id="emailLogin" type="email" class="validate"></input>
-//               <label for="email">Email</label>
-//             </div>
-//           </div>
-
-//           <div class="row password">
-//             <div class="input-field col s6 push-s3">
-//               <input id="passwordLogin" type="password" class="validate"></input>
-//               <label for="password">Password</label>
-//             </div>
-//           </div>
-
-//           <div className="title">
-//               <button class="waves-effect waves-light btn"><i class="material-icons right"></i>Log In</button>
-//           </div>
-
-//         </form>
-
-//       </div>
-
-//     </div>
+    );
+}
